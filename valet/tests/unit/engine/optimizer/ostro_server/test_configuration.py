9700037a98c16eacaabc8228161d861945db1f2d
Chris <cm876n@att.com>
2017-05-24 15:14:39 -0500

    [WIP] Engine unit test stub
    Stubbing out structure for engine unit testing
    directories. Add apache license and empty classes.
    
    Change-Id: I3d4290a40757da82a47bd400238c082e00d5b9e4

--- valet/tests/unit/engine/optimizer/ostro_server/test_configuration.py
+++ valet/tests/unit/engine/optimizer/ostro_server/test_configuration.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test Config."""
+"""Test Configuration."""
 
 import sys
 from valet.engine.optimizer.ostro_server.configuration import Config
