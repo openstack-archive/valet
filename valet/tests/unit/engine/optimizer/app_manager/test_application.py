3c840d55f816b0df5a55086721af0fe532df473a
Chris <cm876n@att.com>
2017-06-12 14:00:49 -0500

    Unit Testing optimizer/app_manager
    Adds unit tests and framework for engine/optimizer/app_manager
    
    Story: #2001039
    Task: #4595
    Change-Id: I4595b832de3a868db2b680679e8fa4dd06a8cdfd

--- valet/tests/unit/engine/optimizer/app_manager/test_application.py
+++ valet/tests/unit/engine/optimizer/app_manager/test_application.py
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test Application."""
-
+from valet.engine.optimizer.app_manager.app_topology_base import VGroup
+from valet.engine.optimizer.app_manager.app_topology_base import VM
+from valet.engine.optimizer.app_manager.application import App
 from valet.tests.base import Base
 
 
@@ -22,3 +23,40 @@ class TestApplication(Base):
 
     def setUp(self):
         super(TestApplication, self).setUp()
+
+        self.app = App("test_id", "test_name", "test_action")
+
+    def test_add_vm(self):
+        vm = VM("app_uuid", "uuid")
+
+        self.app.add_vm(vm, "test_host", "test_status")
+        self.assertEqual(vm, self.app.vms[vm.uuid])
+
+    def test_add_vgroup(self):
+        vgroup = VGroup("app_uuid", "uuid")
+
+        self.app.add_vgroup(vgroup, "test_host")
+        self.assertEqual(vgroup, self.app.vgroups[vgroup.uuid])
+
+    def test_get_json_info(self):
+        request_type = "test_action"
+        timestamp_scheduled = "test_timestamp"
+        app_id = "test_stack_id"
+        app_name = "test_app_name"
+
+        self.app.request_type = request_type
+        self.app.timestamp_scheduled = timestamp_scheduled
+        self.app.app_id = app_id
+        self.app.app_name = app_name
+
+        test_json = {
+            'action': request_type,
+            'timestamp': timestamp_scheduled,
+            'stack_id': app_id,
+            'name': app_name,
+            'VMs': {},
+            'VGroups': {}
+        }
+
+        result = self.app.get_json_info()
+        self.assertEqual(test_json, result)
