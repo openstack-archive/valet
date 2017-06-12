d63cba48f0c96d9a2fe01cd761f99c68d081d07d
Chris <cm876n@att.com>
2017-06-12 14:00:49 -0500

    Unit Testing optimizer/app_manager
    Adds unit tests and framework for engine/optimizer/app_manager
    
    Story: #2001039
    Task: #4595
    Change-Id: I4595b832de3a868db2b680679e8fa4dd06a8cdfd

--- valet/tests/unit/engine/optimizer/app_manager/test_app_handler.py
+++ valet/tests/unit/engine/optimizer/app_manager/test_app_handler.py
@@ -13,8 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit Tests App Handler."""
+import mock
+import uuid
 
+from valet.engine.optimizer.app_manager.app_handler import AppHandler
+from valet.engine.optimizer.app_manager.app_handler import AppHistory
+from valet.engine.optimizer.app_manager.application import App
+from valet.engine.optimizer.db_connect.music_handler import MusicHandler
 from valet.tests.base import Base
 
 
@@ -22,3 +27,186 @@ class TestAppHandler(Base):
 
     def setUp(self):
         super(TestAppHandler, self).setUp()
+
+        rstr = uuid.uuid4().hex
+
+        self.config = mock.Mock()
+        self.logger = mock.Mock()
+        self.db = MusicHandler(self.config, self.logger)
+        self.app = AppHandler(rstr, self.db, self.config, self.logger)
+
+    def test_check_history_no_action(self):
+        mock_app = {
+            "stack_id": "foo",
+            "action": "bar",
+        }
+
+        result = self.app.check_history(mock_app)
+        self.assertEqual(result, (None, None))
+
+    def test_check_history_create(self):
+        app_history_mock = AppHistory("none")
+        mock_app = {
+            "stack_id": "foo",
+            "action": "create",
+        }
+        mock_key = "foo:create:none"
+
+        app_history_mock.decision_key = mock_key
+        app_history_mock.result = "bar"
+        self.app.decision_history[mock_key] = app_history_mock
+
+        result = self.app.check_history(mock_app)
+        self.assertEqual(result, (mock_key, "bar"))
+
+        self.app.decision_history = {}
+        result = self.app.check_history(mock_app)
+        self.assertEqual(result, (mock_key, None))
+
+    def test_check_history_replan(self):
+        app_history_mock = AppHistory("none")
+        mock_app = {
+            "stack_id": "foo",
+            "action": "replan",
+            "orchestration_id": "bar",
+        }
+        mock_key = "foo:replan:bar"
+
+        app_history_mock.decision_key = mock_key
+        app_history_mock.result = "result"
+        self.app.decision_history[mock_key] = app_history_mock
+
+        result = self.app.check_history(mock_app)
+        self.assertEqual(result, (mock_key, "result"))
+
+        self.app.decision_history = {}
+        result = self.app.check_history(mock_app)
+        self.assertEqual(result, (mock_key, None))
+
+    @mock.patch.object(AppHandler, '_clean_decision_history')
+    def test_put_history_create(self, mock_clean):
+        key = "foo:create:bar"
+        mock_app_history = AppHistory(key)
+        mock_app_history.result = "result"
+
+        self.app.max_decision_history = 0
+
+        self.app.put_history(key, "result")
+        self.assertEqual(self.app.decision_history[key].result, "result")
+        self.assertEqual(self.app.decision_history[key].decision_key, key)
+        mock_clean.assert_called_once_with()
+
+    @mock.patch.object(AppHandler, '_clean_decision_history')
+    def test_put_history_replan(self, mock_clean):
+        key = "foo:replan:bar"
+        mock_app_history = AppHistory(key)
+        mock_app_history.result = "result"
+
+        self.app.max_decision_history = 0
+
+        self.app.put_history(key, "result")
+        self.assertEqual(self.app.decision_history[key].result, "result")
+        self.assertEqual(self.app.decision_history[key].decision_key, key)
+
+    def test_clean_decision_history(self):
+        self.app.min_decision_history = 0
+        app_history_one = AppHistory("keyone")
+        app_history_two = AppHistory("keytwo")
+        app_history_one.timestamp = 1
+        app_history_two.timestamp = 2
+        self.app.decision_history = {
+            "keyone": app_history_one,
+            "keytwo": app_history_two,
+        }
+
+        self.app._clean_decision_history()
+        self.assertEqual(len(self.app.decision_history),
+                         self.app.min_decision_history)
+
+    @mock.patch.object(MusicHandler, 'add_app')
+    @mock.patch.object(App, 'get_json_info')
+    def test_store_app_placement_true(self, mock_get_json, mock_add_app):
+        mock_get_json.return_value = "info"
+        mock_add_app.return_value = True
+        test_app = App("test_id", "test_name", "test_action")
+
+        self.app.apps = {"foo": test_app}
+        result = self.app._store_app_placements()
+        mock_get_json.assert_called_once_with()
+        mock_add_app.assert_called_once_with("foo", "info")
+        self.assertTrue(result)
+
+    @mock.patch.object(MusicHandler, 'add_app')
+    @mock.patch.object(App, 'get_json_info')
+    def test_store_app_placement_false(self, mock_get_json, mock_add_app):
+        mock_get_json.return_value = "info"
+        mock_add_app.return_value = False
+        test_app = App("test_id", "test_name", "test_action")
+
+        self.app.apps = {"foo": test_app}
+        result = self.app._store_app_placements()
+        mock_get_json.assert_called_once_with()
+        mock_add_app.assert_called_once_with("foo", "info")
+        self.assertFalse(result)
+
+    @mock.patch.object(MusicHandler, 'add_app')
+    def test_remove_placement(self, mock_add_app):
+        mock_add_app.return_value = True
+
+        self.app.apps = {"foo": "bar"}
+        self.app.remove_placement()
+        mock_add_app.assert_called_once_with("foo", None)
+
+    @mock.patch.object(MusicHandler, 'get_vm_info')
+    def test_get_vm_info(self, mock_get_vm_info):
+        vm_info = {'vm_info': 'exists'}
+        mock_get_vm_info.return_value = {'vm_info': 'exists'}
+        result = self.app.get_vm_info('foo', 'bar', 'test')
+        self.assertEqual(result, vm_info)
+
+    def test_get_vm_info_none(self):
+        mock_vm_info = {}
+
+        result = self.app.get_vm_info(None, None, None)
+        self.assertEqual(result, mock_vm_info)
+
+        result = self.app.get_vm_info('none', 'none', None)
+        self.assertEqual(result, mock_vm_info)
+
+        result = self.app.get_vm_info('none', 'foo', None)
+        self.assertEqual(result, mock_vm_info)
+
+        result = self.app.get_vm_info('foo', None, None)
+        self.assertEqual(result, mock_vm_info)
+
+    @mock.patch.object(MusicHandler, 'update_vm_info')
+    def test_update_vm_info(self, mock_db):
+        mock_db.return_value = False
+        result = self.app.update_vm_info("foo", "bar")
+        mock_db.assert_called_once_with("foo", "bar")
+        self.assertFalse(result)
+
+    def test_update_vm_info_uuids_none(self):
+        result = self.app.update_vm_info(None, None)
+        self.assertTrue(result)
+
+        result = self.app.update_vm_info('none', 'none')
+        self.assertTrue(result)
+
+        result = self.app.update_vm_info('none', 'foo')
+        self.assertTrue(result)
+
+        result = self.app.update_vm_info('foo', None)
+        self.assertTrue(result)
+
+    @mock.patch.object(MusicHandler, 'get_app_info')
+    def test_regenerate_app_topology_none(self, mock_get_app_info):
+        mock_get_app_info.return_value = None
+
+        result = self.app._regenerate_app_topology(None, None, None, None)
+        self.assertEqual(result, None)
+
+        mock_get_app_info.return_value = {}
+
+        result = self.app._regenerate_app_topology(None, None, None, None)
+        self.assertEqual(result, None)
--- valet/tests/unit/engine/optimizer/app_manager/test_app_topology_base.py
+++ valet/tests/unit/engine/optimizer/app_manager/test_app_topology_base.py
@@ -13,8 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test App Topology Base."""
+import uuid
 
+from valet.engine.optimizer.app_manager.app_topology_base import VGroup
+from valet.engine.optimizer.app_manager.app_topology_base import VM
 from valet.tests.base import Base
 
 
@@ -22,3 +24,107 @@ class TestAppTopologyBase(Base):
 
     def setUp(self):
         super(TestAppTopologyBase, self).setUp()
+
+        rstr = uuid.uuid4().hex
+        self.vgroup = VGroup(rstr, rstr)
+
+        self.name = "test_name"
+        self.status = "test_status"
+        self.vgroup_type = "test_type"
+        self.level = "test_level"
+        self.survgroup = None
+        self.subvgroup_list = []
+        self.diversity_groups = "div_groups"
+        self.exclusivity_groups = "ex_groups"
+        self.availability_zones = "zones"
+        self.extra_specs_list = "specs_list"
+        self.cpus = "test_vcpus"
+        self.mem = "test_mem"
+        self.local_volume = "test_local_volume"
+        self.cpu_weight = "test_cpu_weight"
+        self.mem_weight = "test_mem_weight"
+        self.local_volume_weight = "weight"
+        self.host = "test_host"
+        self.flavor = "test_flavor"
+
+        self.vgroup_test_info = {
+            'name': self.name,
+            'status': self.status,
+            'vgroup_type': self.vgroup_type,
+            'level': self.level,
+            'survgroup': 'none',
+            'subvgroup_list': [],
+            'diversity_groups': self.diversity_groups,
+            'exclusivity_groups': self.exclusivity_groups,
+            'availability_zones': self.availability_zones,
+            'extra_specs_list': self.extra_specs_list,
+            'cpus': self.cpus,
+            'mem': self.mem,
+            'local_volume': self.local_volume,
+            'cpu_weight': self.cpu_weight,
+            'mem_weight': self.mem_weight,
+            'local_volume_weight': self.local_volume_weight,
+            'host': self.host
+        }
+
+        self.vm = VM(rstr, rstr)
+
+        self.vm_test_info = {
+            'name': self.name,
+            'status': self.status,
+            'survgroup': 'none',
+            'diversity_groups': self.diversity_groups,
+            'exclusivity_groups': self.exclusivity_groups,
+            'availability_zones': self.availability_zones,
+            'extra_specs_list': self.extra_specs_list,
+            'flavor': self.flavor,
+            'cpus': self.cpus,
+            'mem': self.mem,
+            'local_volume': self.local_volume,
+            'cpu_weight': self.cpu_weight,
+            'mem_weight': self.mem_weight,
+            'local_volume_weight': self.local_volume_weight,
+            'host': self.host
+        }
+
+    def test_get_json_info_vgroup(self):
+        self.vgroup.name = self.name
+        self.vgroup.status = self.status
+        self.vgroup.vgroup_type = self.vgroup_type
+        self.vgroup.level = self.level
+        self.vgroup.subvgroup = None
+        self.vgroup.subvgroups = {}
+        self.vgroup.diversity_groups = self.diversity_groups
+        self.vgroup.exclusivity_groups = self.exclusivity_groups
+        self.vgroup.availability_zone_list = self.availability_zones
+        self.vgroup.extra_specs_list = self.extra_specs_list
+        self.vgroup.vCPUs = self.cpus
+        self.vgroup.mem = self.mem
+        self.vgroup.local_volume_size = self.local_volume
+        self.vgroup.vCPU_weight = self.cpu_weight
+        self.vgroup.mem_weight = self.mem_weight
+        self.vgroup.local_volume_weight = self.local_volume_weight
+        self.vgroup.host = self.host
+
+        result = self.vgroup.get_json_info()
+        self.assertEqual(self.vgroup_test_info, result)
+
+    def test_get_json_info_vm(self):
+        self.vm.name = self.name
+        self.vm.status = self.status
+        self.vm.survgroup = self.survgroup
+        self.vm.diversity_groups = self.diversity_groups
+        self.vm.exclusivity_groups = self.exclusivity_groups
+        self.vm.availability_zone = self.availability_zones
+        self.vm.extra_specs_list = self.extra_specs_list
+        self.vm.flavor = self.flavor
+        self.vm.vCPUs = self.cpus
+        self.vm.mem = self.mem
+        self.vm.local_volume_size = self.local_volume
+        self.vm.vCPU_weight = self.cpu_weight
+        self.vm.mem_weight = self.mem_weight
+        self.vm.local_volume_weight = self.local_volume_weight
+        self.vm.host = self.host
+
+        result = self.vm.get_json_info()
+        self.assertEqual(self.vm_test_info, result)
