327bddbe2d46d163286cee30c3ef598450f0ff67
Chris <cm876n@att.com>
2017-06-12 15:20:15 -0500

    Unit test stub listener and db_connect
    Add unit test stubs for engine/listener
    and engine/optimizer/db_connect.
    
    Change-Id: Iabfb3e4b3f4c77772626b2dc19604264e8403545
    Story: #123456
    Task: #7890

--- valet/tests/unit/engine/listener/test_listener_manager.py
+++ valet/tests/unit/engine/listener/test_listener_manager.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test Listener Manager."""
-
 from valet.tests.base import Base
 
 
--- valet/tests/unit/engine/optimizer/db_connect/test_music_handler.py
+++ valet/tests/unit/engine/optimizer/db_connect/test_music_handler.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test Music Handler."""
-
 from valet.tests.base import Base
 
 
